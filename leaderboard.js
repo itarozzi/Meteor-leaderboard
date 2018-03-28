3c9c7f7774619a9c64a19137f6df06db4e6d6068
Ivan Tarozzi <itarozzi@gmail.com>
2018-03-28 18:42:20 +0200

    The first Helper.... new  way

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,11 @@ PlayerList = new Mongo.Collection('players');
 if (Meteor.isClient) {
     console.log("Hello world!");
     
-    Template.leaderboard.player = function() {
-        return "Some other text";
-    }
+    Template.leaderboard.helpers ({
+        'player': function(){
+            return "Some other text";
+        }
+
+        
+    });
 }
