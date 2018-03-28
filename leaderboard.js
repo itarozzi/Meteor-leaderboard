69dc2d8919cb98d9bd6d180bfdc2c5df1eabf37b
Ivan Tarozzi <itarozzi@gmail.com>
2018-03-28 18:39:22 +0200

    The first Helper.... deprecated way

--- leaderboard.js
+++ leaderboard.js
@@ -3,4 +3,7 @@ PlayerList = new Mongo.Collection('players');
 if (Meteor.isClient) {
     console.log("Hello world!");
     
+    Template.leaderboard.player = function() {
+        return "Some other text";
+    }
 }
