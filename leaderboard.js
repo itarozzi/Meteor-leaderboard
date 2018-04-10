bcc83eff2ee610e78a05e86f4eaffd1d3feb5f69
Ivan Tarozzi <itarozzi@gmail.com>
2018-04-10 13:37:46 +0200

    template ed elenco nomi da helper

--- leaderboard.js
+++ leaderboard.js
@@ -1,13 +1,13 @@
 PlayerList = new Mongo.Collection('players');
 
+
 if (Meteor.isClient) {
     console.log("Hello world!");
     
     Template.leaderboard.helpers ({
         'player': function(){
-            return "Some other text";
-        }
-
-        
+            return PlayerList.find();
+            
+        }       
     });
 }
