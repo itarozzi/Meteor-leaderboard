bdbebf6df4a585cf347d5116da3ce92029fe85a5
Ivan Tarozzi <itarozzi@gmail.com>
2018-04-10 19:28:38 +0200

    add user login and createdby

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,9 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
     Template.leaderboard.helpers({
         'player': function(){
-            return PlayersList.find();
+                var currentUserId = Meteor.userId();
+                return PlayersList.find({ createdBy: currentUserId },
+                                        { sort: {score: -1, name: 1} });
         },
         'selectedClass': function(){
             var playerId = this._id;
@@ -17,6 +19,7 @@ if(Meteor.isClient){
             return PlayersList.findOne({ _id: selectedPlayer });
 } 
     });
+    
     
     Template.leaderboard.events({
         'click .player': function(){
@@ -35,9 +38,11 @@ if(Meteor.isClient){
         'submit form': function(event){
             event.preventDefault();
             var playerNameVar = event.target.playerName.value;
+            var currentUserId = Meteor.userId();
             PlayersList.insert({
                 name: playerNameVar,
-                score: 0
+                score: 0,
+                createdBy: currentUserId
             });
             event.target.playerName.value = "";
 
