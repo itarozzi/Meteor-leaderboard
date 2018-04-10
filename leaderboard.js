d11326629648391b9d8f7973fad57e78466d5391
Ivan Tarozzi <itarozzi@gmail.com>
2018-04-10 13:53:29 +0200

    events and session to select name

--- leaderboard.js
+++ leaderboard.js
@@ -1,13 +1,26 @@
-PlayerList = new Mongo.Collection('players');
+PlayersList = new Mongo.Collection('players');
 
-
-if (Meteor.isClient) {
-    console.log("Hello world!");
-    
-    Template.leaderboard.helpers ({
+if(Meteor.isClient){
+    Template.leaderboard.helpers({
         'player': function(){
-            return PlayerList.find();
-            
-        }       
+            return PlayersList.find();
+        },
+        'selectedClass': function(){
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedPlayer');
+            if(playerId == selectedPlayer){
+                return "selected"
+            }
+        }  
     });
+    Template.leaderboard.events({
+        'click .player': function(){
+            var playerId = this._id;
+            Session.set('selectedPlayer', playerId);
+        }
+    });
+}
+
+if(Meteor.isServer){
+    // this code only runs on the server
 }
